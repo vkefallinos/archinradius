20d1a0efac6cf6b145d0d684acc9d895c67957cd
vkefallinos <vkefallinos@gmail.com>
2024-01-27 22:25:06 +0000

    fix seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,34 +2,31 @@ import prisma from "../lib/prisma";
 
 async function main() {
   const response = await Promise.all([
-    prisma.User.upsert({
+    prisma.user.upsert({
       where: { email: "rauchg@vercel.com" },
       update: {},
       create: {
-        name: "Guillermo Rauch",
+        username: "Guillermo Rauch",
         email: "rauchg@vercel.com",
-        image:
-          "https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg",
+        encryptedPassword: "asas",
       },
     }),
-    prisma.User.upsert({
+    prisma.user.upsert({
       where: { email: "lee@vercel.com" },
       update: {},
       create: {
-        name: "Lee Robinson",
+        username: "Lee Robinson",
         email: "lee@vercel.com",
-        image:
-          "https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg",
+        encryptedPassword: "asas",
       },
     }),
-    await prisma.User.upsert({
+    await prisma.user.upsert({
       where: { email: "stey@vercel.com" },
       update: {},
       create: {
-        name: "Steven Tey",
+        username: "Steven Tey",
         email: "stey@vercel.com",
-        image:
-          "https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg",
+        encryptedPassword: "asas",
       },
     }),
   ]);
